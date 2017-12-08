5eaaab741c1eebed23d971db212db1d4e11853b6
Lenin <leninmrty@gmail.com>
2017-12-08 11:51:43 +0000

    added validation to article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
-
+  validates :description, presence: true, length: {minimum: 3, maximum: 500}
+  validates :text, presence: true, length: {minimum: 3, maximum: 40}
 
 end
