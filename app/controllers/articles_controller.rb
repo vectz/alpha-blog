bd348abc690538b7d774f33dcf6913ac8ecb9f1f
Lenin <leninmrty@gmail.com>
2017-12-12 02:22:58 +0000

    edit,index,update methods and link_to

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,52 @@
 class ArticlesController < ApplicationController
+  
+  def index
+  @articles = Article.all  
+    
+  end
+ 
   def new
     @article = Article.new
     
   end
   
-  def create
+  
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  
+  
+ def create
     @article = Article.new(article_params)
     if @article.save
-    flash[:notice] = "Article was Successfully Created"
-    redirect_to article_path(@article)
+     flash[:notice] = "Article was Successfully Created"
+     redirect_to article_path(@article)
     else
       render 'new'
+    end
   end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "article was sucessfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+ 
   
   def show
     @article = Article.find(params[:id])
   end
-end
-  
+
   private
-  
-  def article_params
-    params.require(:article).permit(:text, :description)
-  end
-    
+    def article_params
+      params.require(:article).permit(:text, :description)
+    end
     
     
 end
